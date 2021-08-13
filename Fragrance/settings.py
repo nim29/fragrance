fab5f60b19b68b0354db76808e5bc97dbbc00aa4
nim29 <88183762+nim29@users.noreply.github.com>
2021-08-13 04:40:52 -0700

    Update settings.py

--- Fragrance/settings.py
+++ Fragrance/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+ $ heroku config:set DISABLE_COLLECTSTATIC=1
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
