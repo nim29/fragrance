cf9e7e72648701127f5c7a8a06d9ec1302120f12
nim29 <88183762+nim29@users.noreply.github.com>
2021-08-15 02:17:42 -0700

    Update settings.py

--- Fragrance/settings.py
+++ Fragrance/settings.py
@@ -128,6 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
  $ heroku config:set DISABLE_COLLECTSTATIC=1
 
